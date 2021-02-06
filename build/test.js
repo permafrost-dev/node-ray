ade99850b37482cffe29a44ac0c178a3517d8469
Patrick Organ <patrick@permafrost.dev>
2021-02-06 03:34:18 -0500

    update test script

--- build/test.js
+++ build/test.js
@@ -1,12 +1,7 @@
 
-//const ray = require('./Ray').ray;
 const ray = require('./../dist/index.cjs').ray;
-//import { ray } from './../dist/index.mjs');
 
-const sleep = (seconds) => {
-    return usleep(seconds * 1000);
-};
-
+const sleep = (seconds) => usleep(seconds * 1000);
 const usleep = (milliseconds) => {
     const start = new Date().getTime();
     while (new Date().getTime() < start + (milliseconds)) {}
@@ -14,39 +9,11 @@ const usleep = (milliseconds) => {
 
 function main() {
 
-const val = {object: {}};
-val.circularReference = val;
-val[Symbol('foo')] = 'foo';
-val.map = new Map([['prop', 'value']]);
-val.array = [-0, Infinity, NaN, 'test string'];
-
-const t = function(a, b) { console.log(a, b); };
-class Test1 {
-    a = 1;
-}
-
-
-    ray().table([val, t, Test1, new Test1(), {name: 'one', age: 18}, t, 'three', 'four']);
-
     ray().xml('<one><two><three>3333</three></two></one>');
 
     ray('hello world 3!').green();
 
     ray().table(['one', 'two', 'three', 'four']);
-
-    //let c = 'blue';
-    //for(let i = 0; i < 10; i++) {
-    //    setTimeout( () => { ray().count('abc').color(c); c = c === 'blue' ? 'green' : 'blue'; }, i * 1000);
-    //}
-
-    /*
-        let r = await ray('test pause').pause();
-        console.log(r, typeof r);
-
-        if (!r) {
-            return;
-        }
-    */    
 }
 
 main();
