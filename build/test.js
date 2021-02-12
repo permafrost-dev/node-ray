49b075b264b9d2092cb5ffb16b9a99e3a3a2a539
Patrick Organ <patrick@permafrost.dev>
2021-02-11 22:44:13 -0500

    measure() tests

--- build/test.js
+++ build/test.js
@@ -9,11 +9,26 @@ const usleep = (milliseconds) => {
 
 function main() {
 
-    ray().xml('<one><two><three>3333</three></two></one>');
+    ray().measure('main');
 
-    ray('hello world 3!').green();
 
-    ray().table(['one', 'two', 'three', 'four']);
+    usleep(300);
+
+    ray().measure('main');
+
+    usleep(400);
+
+    ray().measure('main');
+
+    usleep(1100);
+
+    ray().measure('main');
+
+    //ray().xml('<one><two><three>3333</three></two></one>');
+
+    //ray('hello world 3!').green();
+
+    //ray().table(['one', 'two', 'three', 'four']);
 }
 
 main();
