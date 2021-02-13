f9dfdcfaf89cd81caf0d712717a7f59b3d235adf
Patrick Organ <patrick@permafrost.dev>
2021-02-12 22:34:32 -0500

    fix frame filename for caller test snapshot

--- tests/TestClasses/FakeClient.ts
+++ tests/TestClasses/FakeClient.ts
@@ -33,7 +33,7 @@ export class FakeClient extends Client {
             }
 
             if (payload.getType() === 'caller') {
-                payload.data.origin.file = 'Ray.ts';
+                payload.data.content.frame.file_name = 'Ray.ts';
                 payload.data.content.frame.method = 'caller';
             }
 
